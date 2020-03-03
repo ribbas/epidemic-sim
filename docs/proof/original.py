daf8578d57b81fd61a75d2768684e234034a8c12
sabbirahm3d <sabbir0ahmed0@gmail.com>
2020-03-03 16:51:46 -0500

    update original proof of concept script

--- docs/proof/original.py
+++ docs/proof/original.py
@@ -1,78 +1,51 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 import math
 import random
 import sys
 
-SEED = int(sys.argv[-1])
-random.seed(SEED)
-LIMIT = random.randint(3, 1000)
-
-
-def rand_float():
-    return 1 / random.randint(2, LIMIT)
-
-
-def mutations(lethality, infectivity, cure, research):
-    MUTATED_GENE = random.randint(0, 8)
-    if str(lethality)[-1] == str(MUTATED_GENE + 1):
-        return abs(cure - research), infectivity
-    elif str(lethality)[-1] == str(MUTATED_GENE):
-        return cure, abs(infectivity - rand_float())
-    else:
-        return cure, infectivity
-
-
 POPULATION_TOTAL = 7760000000
-POPULATION_HEALTHY = POPULATION_TOTAL
-POPULATION_INFECTED = 0
-TOTAL_INFECTED = 0
-POPULATION_DEAD = 0
-TOTAL_DEAD = 0
-POPULATION_AFFECTED = 0
-
-BIRTH_RATE = rand_float()
-
-SEVERITY = rand_float()  # rate of detection
-LETHALITY = rand_float()  # rate of death
-INFECTIVITY = rand_float()  # rate of infection
-
 CURE = 0.00
-CURE_THRESHOLD = math.ceil(POPULATION_TOTAL * SEVERITY * BIRTH_RATE)
-
 TIME = 0
 
-while POPULATION_HEALTHY > 0 and CURE < 100:
+while CURE < 100.00:
 
-    INFECTIVITY = min(INFECTIVITY + rand_float(), 0.99)
-    LETHALITY = min(LETHALITY + rand_float(), 0.99)
-    RESEARCH = rand_float()
-    if TOTAL_INFECTED > CURE_THRESHOLD:
-        CURE += RESEARCH
-        CURE, INFECTIVITY = mutations(LETHALITY, INFECTIVITY, CURE, RESEARCH)
-    POPULATION_INFECTED = min(POPULATION_HEALTHY, math.ceil(
-        random.randint(1, 100) * math.exp(INFECTIVITY)))
-    TOTAL_INFECTED += POPULATION_INFECTED
-    POPULATION_DEAD = math.ceil(POPULATION_INFECTED * LETHALITY)
-    TOTAL_DEAD += POPULATION_DEAD
-    POPULATION_HEALTHY = POPULATION_HEALTHY - POPULATION_DEAD - \
-                         POPULATION_INFECTED + random.randint(1, 100) * math.ceil(math.exp(BIRTH_RATE))
-    if POPULATION_HEALTHY < 1:
-        POPULATION_HEALTHY += POPULATION_DEAD + POPULATION_INFECTED
-        print("\033[91mHUMANITY ERADICATED\033[00m")
-        break
-    print(
-        f"TIME: {TIME}, CURE: {CURE}, INFECTED: {POPULATION_INFECTED}, DEAD: {POPULATION_DEAD}, HEALTHY: {POPULATION_HEALTHY}")
+    if TIME == 0:
+
+        random.seed(int(sys.argv[-1]))
+        LIMIT = random.randint(100, 1023)
+
+        POPULATION_INFECTED = 0
+        TOTAL_INFECTED = 0
+        POPULATION_DEAD = 0
+        BIRTH_RATE = 1 / random.randint(100, LIMIT)
+        SEVERITY = 1 / random.randint(100, LIMIT)  # rate of detection
+        FATALITY = 1 / random.randint(100, LIMIT)  # rate of death
+        INFECTIVITY = 1 / random.randint(100, LIMIT)  # rate of infection
+        CURE_THRESHOLD = math.floor(POPULATION_TOTAL * SEVERITY * BIRTH_RATE) / LIMIT
+
+    else:
+
+        INFECTIVITY = min(0.25, INFECTIVITY + (1 / random.randint(100, LIMIT)))
+        FATALITY = min(0.25, FATALITY + (1 / random.randint(100, LIMIT)))
+
+        if TOTAL_INFECTED > CURE_THRESHOLD:
+
+            RESEARCH = 1 / random.randint(2, 100)
+            CURE += RESEARCH
+
+            MUTATED_GENE = random.randint(0, 8)
+            MUTATED_GENE1 = MUTATED_GENE + 1
+
+            if str(FATALITY)[-1] == str(MUTATED_GENE1):
+                CURE = abs(CURE - RESEARCH)
+
+            elif str(FATALITY)[-1] == str(MUTATED_GENE):
+                INFECTIVITY = abs(INFECTIVITY - RESEARCH)
+
+        BATCH_INFECTED = random.randint(1, 10) * TIME / LIMIT + 1
+        POPULATION_INFECTED = math.floor(BATCH_INFECTED * INFECTIVITY)
+        POPULATION_DEAD = math.floor(POPULATION_INFECTED * FATALITY)
+
+        TOTAL_INFECTED += POPULATION_INFECTED
+
+    print(f"TIME: {TIME}, CURE: {CURE}, INFECTED: {POPULATION_INFECTED}, DEAD: {POPULATION_DEAD}")
     TIME += 1
-
-print(f"SEED: {SEED}")
-print(
-    f"INFECTIVITY: {INFECTIVITY}, LETHALITY: {LETHALITY}, SEVERITY: {SEVERITY}, THRESHOLD: {CURE_THRESHOLD}")
-print(f"INFECTED: {TOTAL_INFECTED} ({round(TOTAL_INFECTED / POPULATION_TOTAL * 100, 2)}%)")
-print(f"DEAD: {TOTAL_DEAD} ({round(TOTAL_DEAD / POPULATION_TOTAL * 100, 2)}%)")
-print(
-    f"INFECTED BUT ALIVE: {TOTAL_INFECTED - TOTAL_DEAD} ({round((TOTAL_INFECTED - TOTAL_DEAD) / POPULATION_TOTAL * 100, 2)}%)")
-print(
-    f"ALIVE: {POPULATION_HEALTHY + (TOTAL_INFECTED - TOTAL_DEAD)} ({round((POPULATION_HEALTHY + (TOTAL_INFECTED - TOTAL_DEAD)) / POPULATION_TOTAL * 100, 2)}%)")
-print(f"HEALTHY: {POPULATION_HEALTHY} ({round(POPULATION_HEALTHY / POPULATION_TOTAL * 100, 2)}%)")
